92d015333779b2b65fd76752674009e608ff7b9e
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-10 16:31:24 -0700

    adding source=qr support

--- js/constants.js
+++ js/constants.js
@@ -1,2 +1,3 @@
 const YEAR = (new Date()).getFullYear()
 const REDIRECT = "https://launch.paywake.net"
+const QR_REDIRECT = "https://launch.paywake.net/?source=qr"
--- js/preload.js
+++ js/preload.js
@@ -1,2 +1,7 @@
 console.log("\u00A9 " + YEAR.toString() + " Paywake Corporation")
-window.location.replace(REDIRECT);
+if ((new URLSearchParams(window.location.search)).get("source")) {
+  window.location.replace(QR_REDIRECT)
+}
+else {
+  window.location.replace(REDIRECT)
+}
