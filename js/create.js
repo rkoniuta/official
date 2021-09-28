e52d0ddfbce8dec3b5dc137f37016eaf39342a33
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-09-28 13:50:14 -0700

    create patch

--- js/create.js
+++ js/create.js
@@ -1,7 +1,12 @@
 $("input").on("keyup", (e) => {
   let n = parseInt(e.target.id.replace(/\D/g,''))
   if ((e.keyCode === 13) && (n !== 2)) {
-    nextScreen()
+    if (n === 4) {
+      verify()
+    }
+    else {
+      nextScreen()
+    }
   }
 })
 
