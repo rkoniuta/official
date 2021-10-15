1c37511872dd5f0b74ef6a6c784067144adb9fad
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-10-14 17:44:07 -0700

    testing schedule ui

--- js/preload/schedule.js
+++ js/preload/schedule.js
@@ -1,3 +1,42 @@
+const SELECTED_DAYS = [false, false, false, false, false, false]
+
+const toggleDay = (obj) => {
+  const index = parseInt(obj.id.split("-")[2])
+  SELECTED_DAYS[index] = (!SELECTED_DAYS[index])
+  if (SELECTED_DAYS[index]) {
+    $(obj).addClass("selected")
+  }
+  else {
+    $(obj).removeClass("selected")
+  }
+}
+
+const slider = (obj) => {
+  const deposit = Math.round(obj.value)
+  document.getElementById("deposit-amount").innerHTML = deposit.toString()
+}
+
+const sliderInit = (obj) => {
+  const steps = 60
+  const finalPosition = (Math.floor(Math.random() * (SLIDER_INIT_MAX - SLIDER_INIT_MIN)) + SLIDER_INIT_MIN)
+  const duration = ((SLIDER_DURATION_MS / SLIDER_INIT_MAX) * finalPosition)
+  let counter = 0
+  obj.value = 5
+  slider(obj)
+  const interval = setInterval(() => {
+    if (counter < steps) {
+      obj.value = (5 + ((finalPosition - 5) * Math.pow((counter / steps), (1 / 3))))
+      slider(obj)
+      counter++
+    }
+    else {
+      obj.value = finalPosition
+      slider(obj)
+      clearInterval(interval)
+    }
+  }, (duration / steps))
+}
+
 /* STRIPE TESTING */
 const elements = stripe.elements()
 const card = elements.create('card', {
--- js/schedule.js
+++ js/schedule.js
@@ -1,2 +1,6 @@
+toggleDay(document.getElementById("day-select-0"))
+toggleDay(document.getElementById("day-select-1"))
+sliderInit(document.getElementById("deposit-slider"))
+
 /* STRIPE TESTING */
 card.mount('#card-element')
