e23b909b1a1be3347751059ea467ce836dd7b4d7
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-10-26 13:18:46 -0700

    adding blank verification page

---
+++ js/preload/verify.js
@@ -0,0 +1,40 @@
+let STREAM = false
+
+const VIDEO_CONSTRAINTS = {
+  video: {
+    width: {
+      min: 720,
+      ideal: 1080,
+      max: 2560,
+    },
+    height: {
+      min: 720,
+      ideal: 1920,
+      max: 2560
+    },
+    facingMode: "environment"
+  }
+}
+
+const malformedCamera = () => {
+  //TODO: Implement
+}
+
+const initVideo = async () => {
+  const videoElement = document.getElementById("stream")
+  if (!("mediaDevices" in navigator && "getUserMedia" in navigator.mediaDevices)) {
+    malformedCamera()
+  }
+  else {
+    try {
+      STREAM = await navigator.mediaDevices.getUserMedia(VIDEO_CONSTRAINTS)
+    }
+    catch (e) {
+      malformedCamera()
+    }
+    if (STREAM) {
+      videoElement.srcObject = STREAM
+      videoElement.play()
+    }
+  }
+}
