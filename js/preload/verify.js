91d56b6db3b68d9584865910c06c7598c631567e
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-10-26 21:48:56 -0700

    adding big text

--- js/preload/verify.js
+++ js/preload/verify.js
@@ -59,11 +59,14 @@ const initVideo = async () => {
 
 const setInstruction = (item = "") => {
   const element = document.getElementById("instructions")
+  const bigElement = document.getElementById("instructions-big")
   if (item.length) {
     element.innerHTML = ("To verify you're out of bed,<br>take a photo of " + item.split(" ")[0]  + " <b>" + item.substring(item.split(" ")[0].length).trim() + "</b>")
+    bigElement.innerHTML = item.substring(item.split(" ")[0].length).trim().toLowerCase()
   }
   else {
     element.innerHTML = ""
+    bigElement.innerHTML = ""
   }
 }
 
