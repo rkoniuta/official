dbf89c897395f38495d62115654d45f0e585bbf2
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-19 11:30:12 -0800

    adding feedback form

--- js/preload/feedback.js
+++ js/preload/feedback.js
@@ -36,9 +36,41 @@ const FEEDBACK = {
     FEEDBACK.container = container
   },
   negative: (factor = 1) => {
-    FEEDBACK.loading()
-    //TODO: Server stuff
-    FEEDBACK.thanks()
+    MODAL.hide()
+    const h3 = document.createElement("h3")
+    h3.innerHTML = "We're sorry to hear that."
+    h3.id = "__FEEDBACK-sorry"
+    const input = document.createElement("textarea")
+    input.placeholder = "How can we improve?"
+    input.id = "__FEEDBACK-sorry-input"
+    input.maxLength = 500
+    let group = document.createElement("div")
+    group.className = "button-group"
+    let goback = document.createElement("button")
+    goback.innerHTML = "Dismiss"
+    goback.className = "transparent"
+    let confirm = document.createElement("button")
+    confirm.innerHTML = "Submit"
+    confirm.id = "__modal-dismiss"
+    group.appendChild(goback)
+    group.appendChild(confirm)
+    goback.onclick = () => {
+      FEEDBACK.loading()
+      MODAL.hide()
+      //TODO: Server stuff
+      FEEDBACK.thanks()
+    }
+    confirm.onclick = () => {
+      FEEDBACK.loading()
+      MODAL.hide()
+      //TODO: Server stuff
+      FEEDBACK.thanks()
+    }
+    MODAL.display([
+      h3,
+      input,
+      group,
+    ])
   },
   positive: (factor = 1) => {
     FEEDBACK.loading()
@@ -57,8 +89,10 @@ const FEEDBACK = {
       img.src = "assets/images/verified.png"
       h1.insertBefore(img, h1.firstChild)
       container.innerHTML = ""
-      $(container).removeClass("loading")
       container.appendChild(h1)
+      setTimeout(() => {
+        $(container).removeClass("loading")
+      },1)
     }
   },
 }
