9b673bd400a550c7638c711388029120679afc03
Jack Mandelkorn <jack@jackmandelkorn.com>
2021-12-30 18:02:06 -0800

    adding practice mode

---
+++ js/practice.js
@@ -0,0 +1,8 @@
+fetchChallenge()
+setInstruction()
+const INIT_VIDEO_INTERVAL = setInterval(() => {
+  if (document.getElementById("__modal")) {
+    initVideo()
+    clearInterval(INIT_VIDEO_INTERVAL)
+  }
+}, 5)
