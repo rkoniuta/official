dd184d687926bc71f39c70d4c423ff3c1b95970c
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-01-23 15:15:50 -0800

    Update settings.js

--- js/preload/settings.js
+++ js/preload/settings.js
@@ -366,10 +366,10 @@ const genWakeups = () => {
     p.className = "transfer-event"
     let pText = ("<b>" + moment(transfer.time).format("MM/DD/YYYY") + " @ " + moment(transfer.time).format("h:mma") + "</b> &mdash; ")
     if (transfer.data.data.type === "BANK") {
-      pText += ("$" + balanceToString(parseInt(transfer.data.data.amount)) + " transferred to bank account (" + transfer.data.data.id + ")")
+      pText += ("$" + balanceToString(parseInt(transfer.data.data.amount)) + " to bank (" + transfer.data.data.id + ")")
     }
     else {
-      pText += ("$" + balanceToString(parseInt(transfer.data.data.amount) - 25) + " transferred to Venmo account (" + transfer.data.data.id + ")")
+      pText += ("$" + balanceToString(parseInt(transfer.data.data.amount) - 25) + " to Venmo (" + transfer.data.data.id + ")")
     }
     p.innerHTML = pText
     $("#transfer-container")[0].appendChild(p)
