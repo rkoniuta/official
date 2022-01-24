3b95cddafe61655e11385bac47fdb59d86ee3cc2
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-01-24 12:19:17 -0800

    adding ajax loader to all pages with heavy loads

--- js/preload/balance.js
+++ js/preload/balance.js
@@ -21,3 +21,35 @@ const fetchBalance = () => {
     }
   })
 }
+
+let __AJAX_STACK = 0
+
+const putAjaxLoader = () => {
+  const loader = document.createElement("img")
+  loader.src = ("./assets/images/loader-white.svg")
+  loader.id = "__ajax-loader"
+  $(".balance-container")[0].querySelector("h3").appendChild(loader)
+}
+
+const killAjaxLoader = () => {
+  $("#__ajax-loader").remove()
+}
+
+$(document).ajaxStart(() => {
+  if (__AJAX_STACK === 0) {
+    putAjaxLoader()
+  }
+  __AJAX_STACK++;
+})
+$(document).ajaxComplete(() => {
+  __AJAX_STACK--;
+  if (__AJAX_STACK < 1) {
+    killAjaxLoader()
+  }
+})
+$(document).ajaxError(() => {
+  __AJAX_STACK--;
+  if (__AJAX_STACK < 1) {
+    killAjaxLoader()
+  }
+})
