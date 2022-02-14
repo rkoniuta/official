b9801b71c1c4a4997e720bf28f6f3bc3cd4d4ece
Jack Mandelkorn <jack@jackmandelkorn.com>
2022-02-14 12:29:35 -0800

    Update create.js

--- js/preload/create.js
+++ js/preload/create.js
@@ -143,23 +143,8 @@ const verify = () => {
         else {
           const firstName = localStorage.getItem(LOCAL_STORAGE_TAG + "temp-name").split(" ")[0].trim()
           document.getElementById("first-name").innerHTML = (", " + firstName)
-          localStorage.setItem(LOCAL_STORAGE_TAG + "screen", (6).toString())
-          ROUTINES.login(
-            localStorage.getItem(LOCAL_STORAGE_TAG + "temp-username"),
-            localStorage.getItem(LOCAL_STORAGE_TAG + "temp-password"),
-            RECAPTCHA_TOKEN,
-            (err) => {
-              if (err) {
-                localStorage.setItem(LOCAL_STORAGE_TAG + "screen", (5).toString())
-                setScreen(5)
-              }
-              else {
-                localStorage.removeItem(LOCAL_STORAGE_TAG + "temp-username")
-                localStorage.removeItem(LOCAL_STORAGE_TAG + "temp-password")
-                setScreen(6)
-              }
-            }
-          )
+          localStorage.setItem(LOCAL_STORAGE_TAG + "screen", (5).toString())
+          setScreen(5)
         }
       })
     }
